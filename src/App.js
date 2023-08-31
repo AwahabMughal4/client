d33dbd0f1e05ae3d3cdb1f8f920935c0da6c0e95
abdul wahab <abdulwahabmughal4832@gmail.com>
2023-08-31 02:27:01 -0700

    table6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/data')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+        
+      });
+      
+      
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1>Data from SQL Database</h1>
+      
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>GPO_NAME</th>
+            <th>post_code</th>
+            <th>account_office</th>
+            <th>province</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.non_delivery_post_office}</td>
+              <td>{item.post_code}</td>
+              <td>{item.account_office}</td>
+              <td>{item.province}</td>
+              
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
+
+
 export default App;
+
+
+
+
+
+
+
+
+
